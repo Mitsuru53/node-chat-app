c3085b8644c01ed7fdaa26e1a5a3ac933256f37a
Mitsuru Hashimoto <exhikkii@gmail.com>
2017-12-30 13:03:00 +0900

    EMit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: "Mitsu",
-    text: 'Hey, This is Mistu'
-  });
 });
 
 socket.on('disconnect', function() {
